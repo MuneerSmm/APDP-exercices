6a573d73d1613be7ee54821c7be2c119d8662521
Smm.Muneer <smmaashik@gmail.com>
2024-10-31 11:11:20 +0530

    a

---
+++ Untitled-2.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, name= "Muneer", age = 21, skill ):
+        self. name = name
+        self. age = age
+        self. skill = skill[]
+
+    def Person_info(self):
